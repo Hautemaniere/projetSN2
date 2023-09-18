ada05696520f1d2cf1f1d624329f2f51397fa565
LopesDamien <damienlopes80@gmail.com>
2023-09-18 16:20:54 +0200

    formulaire

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-// script.js
-document.addEventListener("DOMContentLoaded", function () {
-    const gifs = document.querySelectorAll(".gif");
+// FORMULAIRE DE CONNEXION
 
-    gifs.forEach((gif) => {
-        gif.addEventListener("mouseover", () => {
-            gif.src = gif.src.replace(".gif", "-hover.gif");
-        });
+      var login = document.getElementById("loginForm");
+      var inscrire = document.getElementById("signinForm");
+      document.getElementById('connexion').addEventListener("click", function(e){
+        login.style.display = "block";
+        inscrire.style.display = "none";
+        this.style.opacity= "1";
+        document.getElementById("inscrire").style.opacity= "0.5";
 
-        gif.addEventListener("mouseout", () => {
-            gif.src = gif.src.replace("-hover.gif", ".gif");
-        });
-    });
-});
+      });
+      document.getElementById('inscrire').addEventListener("click", function(e){
+        login.style.display = "none";
+        inscrire.style.display = "block";
+        this.style.opacity= "1";
+        document.getElementById("connexion").style.opacity= "0.5";
+      });
